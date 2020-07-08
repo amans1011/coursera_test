a01decadca6d5cf1b61638ca98313207d9325ab2
Aman Singh <aman7068967849@gmail.com>
2020-07-08 12:16:24 +0530

    commit

---
+++ site/JavaScript/script.js
@@ -0,0 +1,8 @@
+var company = new Object();
+company.name = "Facebook";
+company.logo = "F";
+company.ceo = new Object();
+company.ceo.firstName = "Mark";
+company.ceo.favColor = "Blue";
+
+console.log(company);
